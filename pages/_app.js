3ebf2b7dc58f9ffc67384a3b97b1aaa5949c9a02
Souhiir1 <souhirmanai9@gmail.com>
2025-05-07 01:43:25 +0100

    fix import alias

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // pages/_app.js
-import '@/components/Layout.css'; 
-import Layout from '@/components/layout';
+import '../components/Layout.css';
+
+import Layout from '../components/layout';
 
 function MyApp({ Component, pageProps }) {
   return (
