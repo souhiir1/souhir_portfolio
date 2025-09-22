3d5326b864347e984d9fa9ede44c754d30dd2a90
Souhiir1 <souhirmanai9@gmail.com>
2025-09-22 18:24:39 +0100

    fix error build

--- pages/index.js
+++ pages/index.js
@@ -314,11 +314,11 @@ const Home = () => {
           </div>
 
          <p className={styles.subheading}>
-  <span className={styles.highlight}>Fondatrice d'Inova Sphere</span> et ingénieure en développement full-stack passionnée par l'innovation technologique.
+  <span className={styles.highlight}>{`Fondatrice d'Inova Sphere`}</span>{`et ingénieure en développement full-stack passionnée par l'innovation technologique.`}
   <br /><br />
-  Spécialisée dans la création de solutions digitales robustes (ERP, SaaS, applications web/mobile) avec une expertise en <span className={styles.highlight}>React.js, Node.js, Python et IA</span>.
+  {`Spécialisée dans la création de solutions digitales robustes (ERP, SaaS, applications web/mobile) avec une expertise en `}<span className={styles.highlight}>React.js, Node.js, Python et IA</span>.
   <br /><br />
-  J'ai dirigé des projets augmentant la performance système de 15-20% et l'engagement utilisateur de 40%, en appliquant les méthodologies <span className={styles.highlight}>Agile</span> et les meilleures pratiques DevOps.
+  {`J'ai dirigé des projets augmentant la performance système de 15-20% et l'engagement utilisateur de 40%, en appliquant les méthodologies`} <span className={styles.highlight}>Agile</span> et les meilleures pratiques DevOps.
 </p>
 
           
